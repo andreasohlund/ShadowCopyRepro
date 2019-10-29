2aefdfd6efd84d27f3a00fb82551556d4444e03c
Andreas Öhlund <andreas.ohlund@particular.net>
2019-10-29 14:41:30 +0100

    First draft

---
+++ MyApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using NServiceBus;
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Initializing");
+
+        var ec = new EndpointConfiguration("MyEndpoint");
+
+        ec.UseTransport<LearningTransport>();
+
+        var endpoint = Endpoint.Start(ec).GetAwaiter().GetResult();
+        Console.WriteLine("Running");
+
+        Thread.Sleep(TimeSpan.FromSeconds(10));
+
+        endpoint.Stop().GetAwaiter().GetResult();
+
+        Console.WriteLine("Stopped");
+    }
+}
+
+public class SomeThingThatGetScanned : INeedInitialization
+{
+    public void Customize(EndpointConfiguration configuration)
+    {
+        Console.WriteLine("Scanned type invoked");
+    }
+}
---
+++ ShadowCopyRepro/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+static class Program
+{
+    //[LoaderOptimization(LoaderOptimization.MultiDomainHost)]
+    //[STAThread]
+    static void Main()
+    {
+        // Get the startup path. Both assemblies (Loader and
+        // MyApplication) reside in the same directory:
+        var startupPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        // cache path = directory where the assemblies get
+        // (shadow) copied:
+        var cachePath = Path.Combine(startupPath, "__cache");
+
+        var assemblyPath = Path.Combine(startupPath, "MyApplication.exe");
+
+        var setup = new AppDomainSetup
+        {
+            ApplicationName = "MyApplication",
+            ShadowCopyFiles = "true", // note: it isn't a bool
+            CachePath = cachePath
+        };
+
+        var domain = AppDomain.CreateDomain("MyApplication", AppDomain.CurrentDomain.Evidence, setup);
+
+        var lockChecker = new Timer(_ => Console.WriteLine(IsFileLocked(assemblyPath)));
+
+        lockChecker.Change(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));
+
+        domain.ExecuteAssembly(assemblyPath);
+
+        Console.WriteLine();
+
+        AppDomain.Unload(domain);
+
+        if (Directory.Exists(cachePath))
+        {
+            Directory.Delete(cachePath, true);
+        }
+
+        lockChecker.Dispose();
+    }
+
+    static bool IsFileLocked(string path)
+    {
+        FileStream stream = null;
+
+        var file = new FileInfo(path);
+        try
+        {
+            stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+        }
+        catch (IOException)
+        {
+            //the file is unavailable because it is:
+            //still being written to
+            //or being processed by another thread
+            //or does not exist (has already been processed)
+            return true;
+        }
+        finally
+        {
+            if (stream != null)
+                stream.Close();
+        }
+
+        //file is not locked
+        return false;
+    }
+}
